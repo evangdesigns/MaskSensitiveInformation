d434c4f13389a7c97509f41046301f1acc975c62
EvanG <evangdesigns@gmail.com>
2020-03-07 13:20:06 -0600

    done.

--- MaskSensitiveInformation/Program.cs
+++ MaskSensitiveInformation/Program.cs
@@ -6,7 +6,21 @@ namespace MaskSensitiveInformation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tell me your secrets");
+            var secret = Console.ReadLine();
+            var ast = '*';
+
+            if (secret.Length > 4)
+            {
+                Console.WriteLine(
+                    string.Concat(
+                    "".PadLeft(secret.Length - 4, ast),
+                    secret.Substring(secret.Length - 4)
+                    )
+                );
+            }
+
+            Console.ReadKey();
         }
     }
 }
